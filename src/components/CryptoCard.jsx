f2044d1cc93d1fb34e9ac8d2bd376b6460f4b7ca
Nicolas Johnson <nicolas-sousa2011@hotmail.com>
2021-12-20 22:42:30 -0300

    Cryptocurrencies styles

--- src/components/CryptoCard.jsx
+++ src/components/CryptoCard.jsx
@@ -5,14 +5,14 @@ const CryptoCard = ({ rank, name, iconUrl, price, marketCap, change }) => {
   return (
     <div className="cryptoverse_main-content_cryptocurrencies-container-card">
       <div className="cryptoverse_main-content_cryptocurrencies-container-card-header">
-        <h2>{ rank }</h2>
-        <h2>{ name }</h2>
+        <h6>{ rank }.</h6>
+        <p className="subT-1">{ name }</p>
         <img width="50px" src={ iconUrl } alt="Crypto Flag" />
       </div>
       <div className="cryptoverse_main-content_cryptocurrencies-container-card-infos">
-        <p>Price: {millify(price)}</p>
-        <p>Market Cap: {millify(marketCap)}</p>
-        <p>Daily Change {millify(change)}</p>
+        <p>Price: <span>{millify(price)}</span></p>
+        <p>Market Cap: <span>{millify(marketCap)}</span></p>
+        <p>Daily Change <span>{millify(change)}</span></p>
       </div>    
     </div>
   )
